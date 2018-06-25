6fdc0f4f96d5d8ebfeb1ccbe3905b6bdb0c5b0d4
tmyler <33456246+tmyler@users.noreply.github.com>
2018-06-25 15:45:38 +0100

    Updating deprecated L.Mixin.Events reference

--- src/locationfilter.js
+++ src/locationfilter.js
@@ -91,7 +91,7 @@ L.Control.ButtonContainer = L.Control.extend({
 });
 
 L.LocationFilter = L.Class.extend({
-    includes: L.Mixin.Events,
+    includes: L.Evented,
 
     options: {
         enableButton: {
